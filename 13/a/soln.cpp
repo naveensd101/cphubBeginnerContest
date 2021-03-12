6111c972c9f87e8abe7cf264b831dc344a537603
naveen <naveensd101@gmail.com>
2021-03-13 02:31:24 +0530

    beginner contests initial commit

---
+++ 13/a/soln.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long llp;
+#define mod 1000000007
+#define deb(x) cerr << '\t' << "[" << #x << ": "<< x << "]\n";
+#define f first
+#define s second
+#define pb push_back
+/*
+numeric_limits<llp>::max();
+*/
+void solution();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    llp tc = 1;
+    cin >> tc;
+    for (llp t = 0; t < tc; ++t) {
+		//cout << "Case #" << tc+1 << ": ";
+		solution();
+	}
+    return 0;
+}
+
+void solution() {
+}
